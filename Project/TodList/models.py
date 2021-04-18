42f728fbe8336bb9f21f452c73eef66759b9dd66
hossainchisty <hossain.chisty11@gmail.com>
2021-04-18 20:08:52 +0600

    add sample of database

--- Project/TodList/models.py
+++ Project/TodList/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Board(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255,null=True,blank=True)
+    description = models.TextField(blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Profie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255, blank=True,null=True)
+    # image = models.ImageField(upload_to="")
+    bio = models.TextField()
+
+
+class Dashboard(models.Model):
+    pass
